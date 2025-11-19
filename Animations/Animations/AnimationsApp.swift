ec3645541f95d9e7f42a25c1a9fac8c34c2452de
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-11-19 23:24:25 +0530

    Starting the project setup.

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Mohit Sengar on 19/11/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
