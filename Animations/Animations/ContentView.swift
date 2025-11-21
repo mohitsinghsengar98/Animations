da16e17795d30b1884c29e66dba4f1fc0ccd7f0b
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-11-21 22:43:46 +0530

    Custom view modifier for transition animations.

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -7,22 +7,51 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier{
+    let amount: Double
+    let anchor: UnitPoint
+    
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(amount),anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition{
+    static var pivotLeft: AnyTransition{
+        .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading),
+                  identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+    }
+    
+    static var pivotRight: AnyTransition{
+        .modifier(active: CornerRotateModifier(amount: 90, anchor: .topTrailing),
+                  identity: CornerRotateModifier(amount: 0, anchor: .topTrailing))
+    }
+}
+
 struct ContentView: View {
-    @State private var enable = false
+    @State private var isShowingRed = false
     
     var body: some View {
-        VStack{
-            Button("Tap Me"){
-                withAnimation{
-                    enable.toggle()
-                }
-            }
+        ZStack{
             
-            if(enable){
+            if(!isShowingRed){
+                Rectangle()
+                    .fill(.blue)
+                    .frame(width: 200,height: 200)
+                    .transition(.pivotRight)
+                
+            }
+            if(isShowingRed){
                 Rectangle()
                     .fill(.red)
-                    .frame(width: 200,height: 200)
-                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+                    .frame(width: 200,height:200)
+                    .transition(.pivotLeft)
+            }
+        }.onTapGesture {
+            withAnimation{
+                isShowingRed.toggle()
             }
         }
     }
