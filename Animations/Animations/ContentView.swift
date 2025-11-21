d30ba931c4dbb045d2d1557581cd9717214eb7f1
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-11-21 16:09:26 +0530

    ANIMATION CAN BE APPLIED TO THE VALUE DIRECTLY.

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -11,6 +11,10 @@ struct ContentView: View {
     @State private var animationAmount = 1.0
     var body: some View {
         VStack {
+            Stepper("Scale up the button below", value: $animationAmount.animation(), in: 0...5)
+            
+            Spacer()
+            
             Button("Tap Me"){
                 animationAmount += 1
             }.padding(50)
