9e30b6fcbd65a3a41321c2c6dbc3916f06eb48cc
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-11-21 21:43:17 +0530

    Drag animation.

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,38 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 0.0
-    @State private var enable = false
+    @State private var dragAmount = CGSizeZero
     
     var body: some View {
-            
-        Spacer()
-        Button("Tap Me"){
-            withAnimation(.spring(duration: 1.0, bounce: 0.5)){
-                animationAmount += 360
-            }
-        }.padding(50)
-            .background(.red)
-            .foregroundStyle(.white)
-            .clipShape(.circle)
-            .shadow(radius: 20.0)
-            .rotation3DEffect(.degrees(animationAmount),axis: (x: Double.random(in:0...360), y: Double.random(in:0...360), z: Double.random(in:0...360)))
-        
-        Spacer()
-        Button("Tap Me"){
-            enable.toggle()
-            withAnimation(.spring(duration: 1.0, bounce: 0.5)){
-                animationAmount += 360
-            }
-        }.padding(50)
-            .background(enable ? .red : .blue)
-            .foregroundStyle(.white)
-            .clipShape(/*enable ? AnyShape(.circle) :*/ AnyShape(.rect(cornerRadius:enable ? 60.0 : 10.0)) )
-            .shadow(radius: 20.0)
-            .animation(.spring(duration: 1.0,bounce: 0.7), value:!enable)
-            
-//            .rotation3DEffect(.degrees(animationAmount),axis: (x: Double.random(in:0...360), y: Double.random(in:0...360), z: Double.random(in:0...360)))
-        Spacer()
+        LinearGradient(colors: [ .yellow, .blue], startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300,height: 200)
+            .clipShape(.rect(cornerRadius: 10.0))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                .onChanged{ dragAmount = $0.translation }
+                    .onEnded{ _ in
+                        withAnimation(.bouncy){ // explicitly to the end only
+                            dragAmount = .zero
+                        }
+                    }
+            )
+//            .animation(.bouncy, value: dragAmount)// implicit animation approach for both onchange and onEnded
     }
 }
 
